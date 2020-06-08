b5f4e0705e9bdb35b08d599c827913e55caf5635
JDaniloC <jdsc@cin.ufpe.br>
2020-06-08 17:12:38 -0300

    history, open, payout, humor

--- IQ.py
+++ IQ.py
@@ -1,4 +1,5 @@
 from iqoptionapi.stable_api import IQ_Option
+from datetime import datetime
 import time
 
 class IQ_API:
@@ -44,9 +45,30 @@ class IQ_API:
             self.API.change_balance("PRACTICE")
             print("usando a conta treino")
 
+    def conversor_timestamp(self, timestamp):
+        '''
+        Converte o timestamp para horário normal
+        '''
+        return datetime.fromtimestamp(timestamp)
+
     def perfil(self):
+        '''
+        Mostra o perfil de forma simplificada e devolve o dicionário com mais informações
+        return: dict
+        '''
         profile = self.API.get_profile_ansyc()
-        print(profile)
+        resultado = f"""
+        [Perfil]
+Email: {profile['email']}
+Nome: {profile["name"]}
+TimeZone: {profile["tz"]} {profile["tz_offset"]}
+Diferença de tempo: {profile["timediff"]}\n
+Saldo: {profile["currency_char"]} {profile["balance"]}
+Todas as carteiras:\n"""
+        for tipo in profile['balances']:
+            resultado += tipo['currency'] + ": " + str(tipo["amount"]) + "\n"
+        print(resultado)
+        return profile
 
     def ver_saldo(self):
         '''
@@ -54,3 +76,90 @@ class IQ_API:
         '''
         print(f"Saldo na conta {self.API.get_balance_mode()}:", self.API.get_balance())
         return self.API.get_balance()
+    
+    def velas(self, par, segundos, quantidade):
+        '''
+        Devolve as velas de forma simplificada
+        return:
+            dict list: [{}]
+        '''
+        velas = self.API.get_candles(par, segundos, quantidade, time.time())
+        for vela in velas:
+            print(f"{self.conversor_timestamp(vela['from'])} - {self.conversor_timestamp(vela['to'])}")
+            print(f"abertura: {vela['open']}\nFechamento: {vela['close']}")
+        return velas
+
+    def humor(self, par):
+        '''
+        Devolve o humor ATUAL dos traders
+        '''
+        self.API.start_mood_stream(par)
+        resultado = int(100 * round(self.API.get_traders_mood(par), 2))
+        self.API.stop_mood_stream(par)
+        print(f"{par}: {resultado}")
+        return resultado
+
+    def payout_digital(self, par, timeframe = 1):
+        self.API.subscribe_strike_list(par, timeframe)
+        while True:
+            resultado = self.API.get_digital_current_profit(par, timeframe)
+            if resultado:
+                resultado = int(resultado)
+                break
+            time.sleep(1)
+        self.API.unsubscribe_strike_list(par, timeframe)
+        return resultado
+
+    def abertas(self, tipo = "digital"):
+        '''
+        Exibe todas as paridades abertas
+        Devolve um dicionário onde a chave é a paridade
+        E o valor é a rentabilidade
+        return:
+            dict: tuple
+        '''
+        print(f"\nParidades abertas\n")
+        paridades = self.API.get_all_open_time()
+        payouts = self.API.get_all_profit()
+        abertas = {}
+
+        if tipo != "digital":
+            for par in paridades["turbo"]:
+                if paridades['turbo'][par]["open"]: 
+                    abertas[str(par)] = (payouts[par]['turbo'], payouts[par]['binary'])
+                    print(f"[TURBO] {par} - {int(payouts[par]['turbo'] * 100) if type(payouts[par]['turbo']) != dict else '00'}% {int(payouts[par]['binary'] * 100) if type(payouts[par]['binary']) != dict else '00'}%")
+        else:
+            for par in paridades["digital"]:
+                if paridades['digital'][par]["open"]: 
+                    abertas[str(par)] = self.payout_digital(par)
+                    print(f"[DIGITAL] {par} {self.payout_digital(par)}%")
+
+        return abertas
+    
+    def historico(self, quantidade, tipo = "binary-option"):
+        '''
+        Devolve o histórico das operações anteriores
+        Params:
+            quantidade: int (quantas operações)
+            tipo: binary-option ou turbo-option
+        '''
+        # Como pega a digital?
+        status, historico = self.API.get_position_history_v2(tipo, quantidade, 0, 0, 0)
+        lista = []
+        for operacao in historico['positions']:
+            lucro = operacao['close_profit']
+            resultado = {
+                "paridade": operacao['raw_event']['instrument_underlying'] if tipo == "turbo-binary" else operacao['raw_event']['active'],
+                "abertura":  self.conversor_timestamp(operacao['open_time'] / 1000),
+                "fechamento": self.conversor_timestamp(operacao['close_time'] / 1000),
+                "lucro": lucro if lucro == 0 else round(lucro -  operacao['invest'], 2),
+                "direcao": operacao['raw_event']['instrument_dir'] if tipo == "turbo-option" else operacao['raw_event']['direction']
+            }
+            lista.append(resultado)
+
+        for operacao in lista:
+            for key, value in operacao.items():
+                print(f"{key}: {value}")
+            print()
+
+        return lista
