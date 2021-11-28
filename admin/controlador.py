e43d46b203ef335bb02bf6d432bf53162698a661
Danilo Silva <jdsc@cin.ufpe.br>
2021-11-27 21:38:17 -0300

    update controlador

--- admin/controlador.py
+++ admin/controlador.py
@@ -221,48 +221,6 @@ class Control:
             system(f"gcloud compute ssh {alvo.name} --zone {alvo.region} --command='screen -X -S @{email} stuff \"{comando}\n\"'")
         return "Modo ao vivo não ligado."
 
-@bottle.post("/add/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.adicionar_pessoa(*args, **kwargs)
-    return { "resultado": resultado }
-
-@bottle.post("/stop/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.parar_operacao(*args, **kwargs)
-    return { "resultado": resultado }
-
-@bottle.post("/log/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.pegar_log(*args, **kwargs)
-    return { "resultado": resultado }
-
-@bottle.post("/delete/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.deletar_instancias(*args, **kwargs)
-    return { "resultado": resultado }
-
-@bottle.post("/list/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.mostrar_usuarios(*args, **kwargs)
-    return { "resultado": resultado }
-
-@bottle.post("/send/")
-def adicionar_pessoa():
-    data = json.loads(bottle.request.body.read())
-    args, kwargs = data["args"], data["kwargs"]
-    resultado = controlador.enviar_comando(*args, **kwargs)
-    return { "resultado": resultado }
-
 class Client:
     def __init__(self):
         self.host = "127.0.0.1"
@@ -289,8 +247,3 @@ class Client:
     
     def mostrar_usuarios(self, *args, **kwargs):
         return self.send("list", args, kwargs)
-
-if __name__ == "__main__":
-    controlador = Control()
-    print(f"Server preparado para receber. na porta {SERVER_PORT}")
-    bottle.run(host = "localhost", port = SERVER_PORT, debug = True)
