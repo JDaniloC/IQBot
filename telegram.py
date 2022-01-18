7640f6a27573d1af44c059ba929d8c35486136d7
JDaniloC <jdsc@cin.ufpe.br>
2022-01-18 20:32:01 -0300

    recover merge

--- telegram.py
+++ telegram.py
@@ -477,9 +477,10 @@ EURJPY 31/12/2000 CALL M5 02:30
                  KeyboardButton( text = "Operar Auto VIP")],
                 [KeyboardButton( text = "Catalogar sinais"),
                  KeyboardButton( text = "Operar Chinesa"),
-                 KeyboardButton( text = "Config VIP" )],
-                [KeyboardButton( text = "Lista de sinais" ),
-                 KeyboardButton( text = "Operar Berman" )],
+                 KeyboardButton( text = "Estratégia 3 por 1" )],
+                [KeyboardButton( text = "Operar Donchian" ),
+                 KeyboardButton( text = "Operar Berman" ),
+                 KeyboardButton( text = "Operar Price Action" )],
                 [KeyboardButton( text = "Configurações" ),
                  KeyboardButton( text = "Parar Bot" ),
                  KeyboardButton( text = "Sair da conta" )]
@@ -516,7 +517,7 @@ EURJPY 31/12/2000 CALL M5 02:30
         elif texto == "Operar Donchian":
             self.tipo_operacao = "donchian"
             return self.operar(msg)
-        elif texto == "Config VIP":
+        elif texto == "Estratégia 3 por 1":
             self.tipo_operacao = "3por1"
             return self.operar(msg)
         elif texto == "Operar Price Action":
